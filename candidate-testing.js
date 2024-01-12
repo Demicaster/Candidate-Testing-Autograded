2321d0ef87a6281a00686fcdb7c3e59c24784a6a
Joseph <Demicaster@gmail.com>
2024-01-11 20:41:19 -0600

    Fixed few equations

--- candidate-testing.js
+++ candidate-testing.js
@@ -24,12 +24,18 @@ candidateName = input.question ("what is your name? ");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer == input.question (question) ;
+candidateAnswer = input.question (question) ;
 
 }
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+if (candidateAnswer === correctAnswer) {
+  console.log("Correct!")
+}
+else {
+  console.log("Incorrect.") 
+}
 
 
 
